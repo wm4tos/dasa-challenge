f34386ddcc5d94a9e909e462a38081d76bbb51d2
Wesley Matos <wrickee@gmail.com>
2021-01-27 22:23:39 -0300

    feat: use root typeorm module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'mysql',
+      port: 3306,
+      username: 'root',
+      password: process.env.MYSQL_ROOT_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
+      entities: [join(__dirname, '**', 'entities', '*.entity.ts')],
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
